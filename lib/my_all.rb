6b65674a511448bf5cb4e9e8431a0db3ab066b7b
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 16:42:55 -0400

    commit tree fd55351d90b169b776ece3078292675292f3407c

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
-def my_all?(collection)
+def my_all?( array )
+    count = 0
+    block_return_values = []
+    while count < array.length
+        block_return_values << yield( array[count] )
+        count += 1
+    end
 
+    if block_return_values.include?(false)
+        false
+    else
+        true
+    end
 end
